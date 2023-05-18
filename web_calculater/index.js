b9d5e639c5277816f09c4341e9e3000a962eaa08
namkiyoun <skarldbs321@gmail.com>
2023-05-18 22:19:59 +0900

    계산기 기능 수정(연속계산)

--- web_calculater/index.js
+++ web_calculater/index.js
@@ -1,6 +1,11 @@
 
 
 function send(data){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value="";
+        opt.value="";
+    }
     var display = document.getElementById('ipt');
     display.value=display.value+data;
 }
@@ -10,21 +15,46 @@ function allClear(){
 }
 
 function Power(){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value=opt.value;
+        opt.value="";
+    }
     var display = document.getElementById('ipt');
     var result = eval(display.value*display.value);
     document.getElementById('opt').value = result;
    
 }
 function plus(){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value=opt.value;
+        opt.value="";
+    }
     send("+");
 }
 function minus(){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value=opt.value;
+        opt.value="";
+    }
     send("-");
 }
 function multiply(){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value=opt.value;
+        opt.value="";
+    }
     send("*");
 }
 function divide(){
+    if(ipt.value!="" && opt.value!="")
+    {
+        ipt.value=opt.value;
+        opt.value="";
+    }
     send("/");
 }
 
